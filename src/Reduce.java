914f3890ce170375242b296a35589c444b8d9f38
Vanessa Vistanio <vanessa.vistanio@mail.utoronto.ca>
2024-09-10 13:13:59 -0400

    fixes

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,6 +1,6 @@
 public class Reduce {
-    public static void main(String[] args) {
-        int n = 100;
+    public static void main(int x) {
+        int n = x;
         int steps = 0;
         while (n != 0) {
             if (n % 2 == 0){
